37e07d91eb3aaab86e9a6029abeff55c6ff387d2
Arbaaz <khanarbaj45@.com>
2020-11-18 13:29:04 +0530

    DOCS: id in Student Data

--- local_api/views.py
+++ local_api/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render
 from .models import Student
 from .serializers import StudentSerializer
 from rest_framework.renderers import JSONRenderer
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+
 
 # Create your views here.
 # def Student_detail(request):
@@ -15,12 +16,13 @@ from django.http import HttpResponse
 def Student_detail(request, pk):
     stu = Student.objects.get(id = pk)
     serializer = StudentSerializer(stu)
-    json_data = JSONRenderer().render(serializer.data)
-    return HttpResponse(json_data, content_type='application/json')
-
+    # json_data = JSONRenderer().render(serializer.data)
+    # return HttpResponse(json_data, content_type='application/json')
+    return JsonResponse(serializer.data)
 
 def Student_list(request):
     stu = Student.objects.all()
     serializer = StudentSerializer(stu, many=True)
-    json_data = JSONRenderer().render(serializer.data)
-    return HttpResponse(json_data, content_type='application/json')
+    # json_data = JSONRenderer().render(serializer.data)
+    # return HttpResponse(json_data, content_type='application/json')
+    return JsonResponse(serializer.data, safe=False)
