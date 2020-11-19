0a72487761478e80cb2656a78cb65933d773edaa
Arbaaz <khanarbaj45@.com>
2020-11-19 12:01:51 +0530

    DOCS: Model_Serializer With Read only Method

--- local_api/serializers.py
+++ local_api/serializers.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 from .models import Student
 
 class StudentSerializer(serializers.ModelSerializer):
+    name = serializers.CharField(read_only=True)
     class Meta:
         model = Student
         fields = ['id', 'name', 'roll', 'city']
