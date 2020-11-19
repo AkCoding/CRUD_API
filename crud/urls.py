3615ee548b5665cbcd47dd417afc95684bbb656f
Arbaaz <khanarbaj45@.com>
2020-11-19 21:57:57 +0530

    DOCS: Added OOPS Concept in Generic API and Mixins

--- crud/urls.py
+++ crud/urls.py
@@ -4,9 +4,6 @@ from local_api import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('studentapi/', views.StudentList.as_view()),
-    # path('studentapi/', views.StudentCreate.as_view()),
-    # path('studentapi/<int:pk>', views.StudentRetrive.as_view()),
-    # path('studentapi/<int:pk>', views.StudentUpdate.as_view()),
-    path('studentapi/<int:pk>', views.StudentDestroy.as_view()),
+    path('studentapi/', views.LCStudentAPI.as_view()),
+    path('studentapi/<int:pk>', views.RUDStudentAPI.as_view()),
 ]
--- local_api/views.py
+++ local_api/views.py
@@ -4,41 +4,32 @@ from rest_framework.generics import GenericAPIView
 from rest_framework.mixins import ListModelMixin, CreateModelMixin,\
     RetrieveModelMixin, UpdateModelMixin, DestroyModelMixin
 
-class StudentList(GenericAPIView, ListModelMixin):
+
+# List and Create - PK is not required
+class LCStudentAPI(GenericAPIView, ListModelMixin, CreateModelMixin):
     queryset = Student.objects.all()
     serializer_class = StudentSerializer
 
-    def get(self,request, *args, **kwargs):
+    def get(self, request, *args, **kwargs):
         return self.list(request, *args, **kwargs)
 
-class StudentCreate(GenericAPIView, CreateModelMixin):
-    queryset = Student.objects.all()
-    serializer_class = StudentSerializer
-
-    def post(self,request, *args, **kwargs):
+    def post(self, request, *args, **kwargs):
         return self.create(request, *args, **kwargs)
 
 
-class StudentRetrive(GenericAPIView, RetrieveModelMixin):
+
+
+# Retrieve Update and Destroy - PK is required
+class RUDStudentAPI(GenericAPIView, RetrieveModelMixin, UpdateModelMixin, DestroyModelMixin):
     queryset = Student.objects.all()
     serializer_class = StudentSerializer
 
-    def get(self,request, *args, **kwargs):
+    def get(self, request, *args, **kwargs):
         return self.retrieve(request, *args, **kwargs)
 
-
-class StudentUpdate(GenericAPIView, UpdateModelMixin):
-    queryset = Student.objects.all()
-    serializer_class = StudentSerializer
-
-    def put(self,request, *args, **kwargs):
+    def put(self, request, *args, **kwargs):
         return self.update(request, *args, **kwargs)
 
-
-class StudentDestroy(GenericAPIView, DestroyModelMixin):
-    queryset = Student.objects.all()
-    serializer_class = StudentSerializer
-
-    def delete(self,request, *args, **kwargs):
+    def delete(self, request, *args, **kwargs):
         return self.destroy(request, *args, **kwargs)
 
