00b37daa94b2b9ea460f554662508bc830add117
Arbaaz <khanarbaj45@gmail.com>
2020-11-23 15:59:46 +0530

    feat : Added Session Authentication using DjangoModelPermissionAnonReadOnly

--- local_api/views.py
+++ local_api/views.py
@@ -3,14 +3,14 @@ from .serializers import StudentSerializer
 from rest_framework import status
 from rest_framework import viewsets
 from rest_framework.authentication import BasicAuthentication, SessionAuthentication
-from rest_framework.permissions import IsAuthenticatedOrReadOnly, DjangoModelPermissions
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, DjangoModelPermissionsOrAnonReadOnly
 
 
 class StudentModelViewset(viewsets.ModelViewSet):
     queryset = Student.objects.all()
     serializer_class = StudentSerializer
     authentication_classes = [SessionAuthentication]
-    permission_classes = [DjangoModelPermissions]
+    permission_classes = [DjangoModelPermissionsOrAnonReadOnly]
     # permission_classes = [IsAuthenticated]
     # permission_classes = [AllowAny]
 
