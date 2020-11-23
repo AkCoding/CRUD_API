6b673fd3ef1e01d712ab6d5264cb6e610b1e4acc
Arbaaz <khanarbaj45@gmail.com>
2020-11-23 21:00:23 +0530

    chore : Token Authentication by exposing an API endpoint

--- crud/urls.py
+++ crud/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from local_api import views
 from rest_framework.routers import DefaultRouter
-
+from local_api.auth import CustomAuthToken
 
 
 router = DefaultRouter()
@@ -12,5 +12,6 @@ router.register('studentapi', views.StudentModelViewset, basename='student')
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(router.urls)),
-    path('auth/', include('rest_framework.urls', namespace='rest_framework'))
+    path('auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('gettoken/', CustomAuthToken.as_view())
 ]
--- local_api/views.py
+++ local_api/views.py
@@ -1,15 +1,13 @@
 from .models import Student
 from .serializers import StudentSerializer
 from rest_framework import viewsets
-from rest_framework.authentication import BasicAuthentication, SessionAuthentication
+from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import DjangoModelPermissionsOrAnonReadOnly
 from .custompermissions import MyPermission
 
 class StudentModelViewset(viewsets.ModelViewSet):
     queryset = Student.objects.all()
     serializer_class = StudentSerializer
-    authentication_classes = [SessionAuthentication]
-    permission_classes = [MyPermission]
 
 
 
