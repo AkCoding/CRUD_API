f3596c25148696a77b8c0eaa2d159b117c1dcf03
Arbaaz <khanarbaj45@gmail.com>
2020-11-23 13:13:26 +0530

    feat : Added Basic Authentication and permission class

--- crud/urls.py
+++ crud/urls.py
@@ -2,6 +2,9 @@ from django.contrib import admin
 from django.urls import path, include
 from local_api import views
 from rest_framework.routers import DefaultRouter
+
+
+
 router = DefaultRouter()
 
 router.register('studentapi', views.StudentReadOnlyModelViewset, basename='student')
--- local_api/views.py
+++ local_api/views.py
@@ -2,7 +2,11 @@ from .models import Student
 from .serializers import StudentSerializer
 from rest_framework import status
 from rest_framework import viewsets
-
+from rest_framework.authentication import BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
 class StudentReadOnlyModelViewset(viewsets.ReadOnlyModelViewSet):
     queryset = Student.objects.all()
     serializer_class = StudentSerializer
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated]
+
