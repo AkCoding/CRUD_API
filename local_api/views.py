4c0a3c9946aba6301bb284cbfc79e1a08c9f4eb7
Arbaaz <khanarbaj45@gmail.com>
2020-11-23 21:55:45 +0530

    chore : Token Authentication by exposing an API endpoint using DjangoSignal

--- local_api/views.py
+++ local_api/views.py
@@ -2,13 +2,15 @@ from .models import Student
 from .serializers import StudentSerializer
 from rest_framework import viewsets
 from rest_framework.authentication import TokenAuthentication
-from rest_framework.permissions import DjangoModelPermissionsOrAnonReadOnly
+from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
 from .custompermissions import MyPermission
 
 class StudentModelViewset(viewsets.ModelViewSet):
     queryset = Student.objects.all()
     serializer_class = StudentSerializer
-
+    authentication_classes = [TokenAuthentication]
+    # permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticatedOrReadOnly]
 
 
 
