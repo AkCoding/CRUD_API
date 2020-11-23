d5ad4ea0a61e4ea6a83ae45164ce21867b05c6de
Arbaaz <khanarbaj45@gmail.com>
2020-11-23 17:09:56 +0530

    chore : Use Custom Permission

--- local_api/views.py
+++ local_api/views.py
@@ -1,18 +1,15 @@
 from .models import Student
 from .serializers import StudentSerializer
-from rest_framework import status
 from rest_framework import viewsets
 from rest_framework.authentication import BasicAuthentication, SessionAuthentication
-from rest_framework.permissions import IsAuthenticatedOrReadOnly, DjangoModelPermissionsOrAnonReadOnly
-
+from rest_framework.permissions import DjangoModelPermissionsOrAnonReadOnly
+from .custompermissions import MyPermission
 
 class StudentModelViewset(viewsets.ModelViewSet):
     queryset = Student.objects.all()
     serializer_class = StudentSerializer
     authentication_classes = [SessionAuthentication]
-    permission_classes = [DjangoModelPermissionsOrAnonReadOnly]
-    # permission_classes = [IsAuthenticated]
-    # permission_classes = [AllowAny]
+    permission_classes = [MyPermission]
 
 
 
