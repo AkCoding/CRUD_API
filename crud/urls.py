e46729ebc0d04ee445fb90fc665ace9cc1810cd3
Arbaaz <khanarbaj45@gmail.com>
2020-11-25 19:28:43 +0530

    feat : Added Simple JSON Web Token in DRF

--- crud/urls.py
+++ crud/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from local_api import views
 from rest_framework.routers import DefaultRouter
-
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
 
 
 router = DefaultRouter()
@@ -12,6 +12,7 @@ router.register('studentapi', views.StudentModelViewset, basename='student')
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(router.urls)),
-    path('auth/', include('rest_framework.urls', namespace='rest_framework'))
-
+    path('gettoken/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('verifytoken/', TokenVerifyView.as_view(), name='token_verify')
 ]
