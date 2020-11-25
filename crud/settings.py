7c885bf5fcd570a8dbd5636f431b423ae8ade131
Arbaaz <khanarbaj45@gmail.com>
2020-11-25 20:24:45 +0530

    feat : Added datetime for access and refresh token

--- crud/settings.py
+++ crud/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+from datetime import timedelta
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -127,3 +127,9 @@ STATIC_URL = '/static/'
 #     'DEFAULT_AUTHENTICATION_CLASSES':['rest_framwork.authentication.BasicAuthentication'],
 #     'DEFAULT_PERMISSION_CLASSES':['rest_framwork.permissions.BasicAuthentication']
 # }
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME':timedelta(minutes=10),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+    'ROTATE_REFRESH_TOKEN':True,
+}
