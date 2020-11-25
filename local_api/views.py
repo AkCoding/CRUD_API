cd61e4c93ac68de377c2321759f76031fe0150eb
Arbaaz <khanarbaj45@gmail.com>
2020-11-25 20:16:04 +0530

    feat : Added Simple JSON Web Token using JWT Authentication Class

--- local_api/views.py
+++ local_api/views.py
@@ -4,13 +4,12 @@ from rest_framework import viewsets
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
 from .custompermissions import MyPermission
-
+from rest_framework_simplejwt.authentication import JWTAuthentication
 class StudentModelViewset(viewsets.ModelViewSet):
     queryset = Student.objects.all()
     serializer_class = StudentSerializer
-    authentication_classes = [TokenAuthentication]
-    # permission_classes = [IsAuthenticated]
-    permission_classes = [IsAuthenticatedOrReadOnly]
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
 
 
 
